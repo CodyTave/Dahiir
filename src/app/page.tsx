918453830918f888e493393d9b9128746f25dfe8
oussama dahir <dahiroussama@gmail.com>
2023-11-19 00:37:50 +0100

    19/11

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center  p-24">
-      Main Page
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="font-semibold text-3xl">Dahiir.dev</h1>
+      <p className="font-medium">Very Soon</p>
     </main>
   );
 }
