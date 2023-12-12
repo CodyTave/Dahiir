50fc6bebae2f064b3304ea5cb5217068ed896fd2
oussama dahir <dahiroussama@gmail.com>
2023-12-12 16:40:41 +0100

    12/12

--- src/app/components/NextProject.tsx
+++ src/app/components/NextProject.tsx
@@ -4,9 +4,8 @@ import ArrowLongRight from "@/assets/extra/ArrowLongRight";
 import Link from "next/link";
 
 export default async function NextProject({ current }: { current: string }) {
-  const NextLink = await useRandom(current);
   return (
-    <Link href={NextLink} className="mt-20 ml-auto hover:-mr-5 transall">
+    <Link href="/work/random" className="mt-20 ml-auto hover:-mr-5 transall">
       <ArrowLongRight className="w-40 text-dark-3 " />
       <span className="text-xs text-dark-3/80">Get a random project</span>
     </Link>
