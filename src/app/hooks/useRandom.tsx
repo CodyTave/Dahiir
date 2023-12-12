d0a230da9123e51a6e24505e738cd5ce3f680029
oussama dahir <dahiroussama@gmail.com>
2023-12-12 16:37:12 +0100

    12/12

--- src/app/hooks/useRandom.tsx
+++ src/app/hooks/useRandom.tsx
@@ -1,10 +1,6 @@
 import { getRandomProject } from "../services/api";
 
 export default async function useRandom(slug?: string) {
-  let randProject;
-  do {
-    randProject = await getRandomProject();
-  } while (randProject.slug === slug);
-
+  const randProject = await getRandomProject();
   return `/work/${randProject.slug}`;
 }
--- src/app/work/random/page.tsx
+++ src/app/work/random/page.tsx
@@ -1,9 +1,11 @@
 import useRandom from "@/app/hooks/useRandom";
+import { getRandomProject } from "@/app/services/api";
 import { redirect } from "next/navigation";
 
 export default async function Random() {
-  const NextLink = await useRandom();
-  redirect(NextLink);
+  const NextLink = await getRandomProject().then((res) => {
+    redirect(`/work/${res.slug}`);
+  });
   return (
     <div className="text-center text-5xl mt-10 font-semibold">
       Wait a Sec...
